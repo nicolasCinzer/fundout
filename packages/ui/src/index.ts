78e937b77587be85ce02235617e8600b588ccd97
nicolasCinzer <nicolas.cinzer00@gmail.com>
2026-06-19 15:23:55 -0500

    chore(packages): scaffold @fundout/tokens, @fundout/ui, @fundout/i18n-marketing

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,5 @@
+export { Button, buttonVariants } from "./button";
+export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter, CardAction } from "./card";
+export { Badge, badgeVariants } from "./badge";
+export { Input } from "./input";
+export { cn } from "./lib/cn";
